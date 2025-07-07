b6e088105b7ec0a1e281bcf444698b6fd72239ea
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2025-07-07 15:03:24 +0200

    feat(movies): add isFavorite method

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,12 +1,15 @@
-import { IsBoolean, IsNumber, IsString } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateMovieDto {
+    @IsNotEmpty()
     @IsString()
     title: string;
 
+    @IsNotEmpty()
     @IsString()
     description: string;
 
+    @IsNotEmpty()
     @IsString()
     image_Url: string;
 
