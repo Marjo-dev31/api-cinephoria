a0bfd807af87f55206883d4d6c965198f93fb53f
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2025-10-31 15:09:51 +0100

    fix(movie): fix update movie (date and minimum age)

--- src/init/init.sql
+++ src/init/init.sql
@@ -1,4 +1,4 @@
-INSERT INTO Movie(title, description, image_Url, minimun_Age, genreId ) VALUES
+INSERT INTO Movie(title, description, image_Url, minimum_Age, genreId ) VALUES
 ("I Love Peru","Lancé dans une course effrénée vers le succès, un comédien biscornu abandonne ses plus fidèles alliés. Seul face à lui-même, une vision troublante le percute. Direction le Pérou pour une aventure spirituelle.","iloveperu.jpg", 13, 'e3d87fe4-94de-4f1a-b1c7-57e4230d2858'),
 (),
 ()
@@ -10,7 +10,7 @@ INSERT INTO Movie(title, description, image_Url, minimun_Age, genreId ) VALUES
 --     "title": "I Love Peru",
 --     "description": "Lancé dans une course effrénée vers le succès, un comédien biscornu abandonne ses plus fidèles alliés. Seul face à lui-même, une vision troublante le percute. Direction le Pérou pour une aventure spirituelle.",
 --     "image_Url": "iloveperu.jpg",
---     "minimun_Age": 13,
+--     "minimum_Age": 13,
 --     "is_Favorite": false,
 --     "create_At": "2025-07-08T15:14:09.924Z"
 --   },
@@ -19,7 +19,7 @@ INSERT INTO Movie(title, description, image_Url, minimun_Age, genreId ) VALUES
 --     "title": "The Ugly Stepsister",
 --     "description": "Dans un royaume où la beauté règne en maître, la jeune Elvira doit faire face à une redoutable concurrence pour espérer conquérir le cœur du prince. Parmi les nombreuses prétendantes, se trouve notamment sa demi-sœur, à l'insolente beauté. Pour parvenir à ses fins dans cette impitoyable course au physique parfait, Elvira devra recourir aux méthodes les plus extrêmes...",
 --     "image_Url": "theuglystepsister.webp",
---     "minimun_Age": 16,
+--     "minimum_Age": 16,
 --     "is_Favorite": true,
 --     "create_At": "2025-07-08T15:01:40.880Z"
 --   },
@@ -28,7 +28,7 @@ INSERT INTO Movie(title, description, image_Url, minimun_Age, genreId ) VALUES
 --     "title": "L'accident de piano",
 --     "description": "Magalie est une star du web hors sol et sans morale qui gagne des fortunes en postant des contenus choc sur les réseaux. Après un accident grave survenu sur le tournage d'une de ses vidéos, Magalie s'isole à la montagne avec Patrick, son assistant personnel, pour faire un break. Une journaliste détenant une information sensible commence à lui faire du chantage… La vie de Magalie bascule.",
 --     "image_Url": "l'accidentdepiano.webp",
---     "minimun_Age": 13,
+--     "minimum_Age": 13,
 --     "is_Favorite": false,
 --     "create_At": "2025-07-08T14:56:29.972Z"
 --   },
@@ -37,7 +37,7 @@ INSERT INTO Movie(title, description, image_Url, minimun_Age, genreId ) VALUES
 --     "title": "Jurassic World : Renaissance",
 --     "description": "Cinq ans après JURASSIC WORLD : LE MONDE D’APRÈS, l’environnement de la planète s’est révélé hostile pour la plupart des dinosaures. Ceux qui subsistent vivent dans des zones équatoriales isolées, aux conditions proches de celles de leur ère d’origine. Parmi ces créatures terrifiantes, trois spécimens renferment peut-être la clé d’un remède capable de changer le destin de l’humanité.",
 --     "image_Url": "jurassicworld.jpg",
---     "minimun_Age": 13,
+--     "minimum_Age": 13,
 --     "is_Favorite": false,
 --     "create_At": "2025-07-10T08:58:53.731Z"
 --   }
